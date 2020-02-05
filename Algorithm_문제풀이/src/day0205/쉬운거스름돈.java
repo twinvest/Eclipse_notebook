66ad12db683731a6bcd6f97b7da4be492e151cb7
KimTaeWoo <twinvest@naver.com>
2020-02-06 00:08:04 +0900

    알고리즘 문제풀이

---
+++ Algorithm_문제풀이/src/day0205/쉬운거스름돈.java
@@ -0,0 +1,35 @@
+package day0205;
+
+import java.util.Scanner;
+
+public class 쉬운거스름돈 {
+	static int[] arr = {50000,10000,5000,1000,500,100,50,10};
+	
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		int testcase = scan.nextInt();
+		for(int t=1; t<=testcase; ++t)
+		{
+			int[] count = new int[8];
+			int num = scan.nextInt();
+			for(int i=0; i<8; ++i)
+			{
+				while(num>0)
+				{
+					++count[i];
+					num -= arr[i];
+				}
+				if(num != 0)
+				{
+					num += arr[i];
+					--count[i];	
+				}
+			}
+			System.out.println("#"+t);
+			for(int i=0; i<8; ++i)
+				System.out.print(count[i]+" ");
+			System.out.println();
+		}
+	}
+}
