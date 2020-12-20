1f8daf1e9cabb7b25080be47d9ef464126241439
KimTaeWoo <twinvest@naver.com>
2020-12-21 00:17:12 +0900

    20201220알고리즘

---
+++ Algorithm_문제풀이/src/day1220/왕실의나이트.java
@@ -0,0 +1,34 @@
+package day1220;
+
+import java.util.Scanner;
+
+public class 왕실의나이트 {
+	public static int[][] dir = {
+			 {1,2}
+			,{-1,2}
+			,{-1,-2}
+			,{1,-2}
+			,{2,1}
+			,{2,-1}
+			,{-2,1}
+			,{-2,-1}
+
+	};
+	public static boolean inside(int y, int x) {
+		return (x>=0 && x<8) && (y>=0 && y<8);
+	}
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String pos = scan.next();
+		int x = pos.charAt(0) - 'a';
+		int y = Integer.parseInt(pos.charAt(1)+"")-1;
+		int count = 0;
+		for(int i = 0; i < 8; ++i) {
+			int ny = y+dir[i][0];
+			int nx = x+dir[i][1];
+			if(inside(ny, nx)) ++count;
+		}
+		System.out.println(count);
+	}
+
+}
