045bbbbc556c4eb7e56359eaa9d2ce9b41bc3861
KimTaeWoo <twinvest@naver.com>
2021-04-04 23:01:05 +0900

    20210404 텀프로젝트

--- Algorithm_문제풀이2021/src/day0404/텀프로젝트_시간초과극복.java
+++ Algorithm_문제풀이2021/src/day0404/텀프로젝트_시간초과극복.java
@@ -1,20 +1,22 @@
-package day0403;
+package day0404;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class 텀프로젝트 {
+public class 텀프로젝트_시간초과극복 {
 	static int numOfStudent;
 	static int[] studentArr = new int[100001];
 	static boolean[] visit = new boolean[100001];
-
+	static boolean[] finished = new boolean[100001];
 	static int startV;
 	static int endV;
 	static int ans;
 	/*
-	 * 이 문제의 핵심은 무방향 그래프에서 탐색을 시작한 곳으로 다시 돌아올 수 있느냐 인듯?
+	 * visited 배열은 기존에 풀었던 DFS 문제들에서도 사용하는 배열이였지만 finished배열은 처음 등장하는 배열.
+	 * finished 배열은 더 이상 이 노드를 방문하지 않을 것을 확신하는 경우에만 true가 된다.
+	 *
 	 */
 	public static void main(String[] args) throws NumberFormatException, IOException {
 
@@ -28,43 +30,34 @@ public class 텀프로젝트 {
 			for(int j =1; j<=numOfStudent; ++j) {
 				studentArr[j] = Integer.parseInt(st.nextToken());
                 visit[j] = false;
+                finished[j] = false;
 			}
 
 			//1번학생부터 탐색시작.
 			for(int j =1; j<=numOfStudent; ++j) {
-				visitNum = 0;
 				startV = j;
 				if(!visit[j]) {
 					dfs(j);
-					visit[j] = true;
 				}
 			}
 			System.out.println(numOfStudent - ans);
 		}
 	}
-	static int visitNum = 0;
-	static boolean flag = false;
-	static boolean dfs(int vertex) {
-		//순환을 이뤘을 때
-		flag = false;
-		if(visit[vertex] && startV == studentArr[endV]) {
-			ans += visitNum;
-			flag = true;
-			return flag;
-		}
-		//방문하지 않았다면
-		if(!visit[vertex]) { //!visit2[studentArr[vertex]]) {
-			//방문하고
-			visit[vertex] = true;
-			++visitNum;
-			endV = vertex;
-			flag = dfs(studentArr[vertex]);
-			if(!flag) {
-				visit[vertex] = false;
-				//flag = true;
-				//return flag;
+
+	static void dfs(int now) {
+		visit[now] = true;
+		int nextVertex = studentArr[now];
+		if(!visit[nextVertex]) {
+			dfs(nextVertex);
+		} else {
+			if(!finished[nextVertex]) {
+				//팀원을 모두 센다
+				for(int i=nextVertex; i!=now; i=studentArr[i]) {
+					++ans;
+				}
+				++ans; //자기 자신을 센다
 			}
 		}
-		return flag;
+		finished[now] = true; //더 이상 해당 노드를 방문할 일이 없다
 	}
 }
