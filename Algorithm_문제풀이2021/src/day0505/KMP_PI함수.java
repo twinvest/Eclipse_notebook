78389a198b8989ebbc6070e1ff5bd93fda21ea67
KimTaeWoo <twinvest@naver.com>
2021-05-06 00:06:22 +0900

    20210506 kmp알고리즘 .....이해하는데 너무 오래걸렸다 후후...

---
+++ Algorithm_문제풀이2021/src/day0505/KMP_PI함수.java
@@ -0,0 +1,40 @@
+package day0505;
+
+import java.util.Arrays;
+
+public class KMP_PI함수 {
+
+	public static void main(String[] args) {
+		System.out.println(Arrays.toString(getPi2("abacaaba")));
+	}
+	public static int[] getPi(String pattern) {  // 접두사와 접미사 매칭 Count
+		int m = pattern.length();     // 찾을 Pattern's Length
+		int []pi = new int[m];        // pi[] 할당
+		int j = 0;                               // 패턴을 탐색할 인덱스
+
+		for(int begin = 1; begin < m; ++begin) {
+			for(int i = 0; i + begin < m; ++i) {
+				if(pattern.charAt(begin + i) != pattern.charAt(i))
+					break;
+				pi[begin + i] = Math.max(pi[begin+i], i+1);
+			}
+		}
+		return pi;     // pi[] 반환
+	}
+
+	public static int[] getPi2(String pattern) {  // 접두사와 접미사 매칭 Count
+		int LenOfPattern = pattern.length();     // 찾을 Pattern's Length
+		int []pi = new int[LenOfPattern];        // pi[] 할당
+		int j = 0;                               // 패턴을 탐색할 인덱스
+
+		for(int i=1; i<LenOfPattern; i++) {      // i = 1부터
+			while(j > 0 && pattern.charAt(i) != pattern.charAt(j)) { // KMP알고리즘, 매칭되는 것 건너뛴다
+				j = 0;//pi[j-1];  ㅁㅊㄴ들이 개같이 pi[j-1]이라 하는지 모르겠네;; // j를 재할당 (건너 뜀)
+			}
+			if(pattern.charAt(i) == pattern.charAt(j)) {            // 접두사랑 접미사가 같다면
+				pi[i] = ++j;                                          // 해당 인덱스 카운트  (길이)
+			}
+		}
+		return pi;     // pi[] 반환
+	}
+}
--- Algorithm_문제풀이2021/src/day0505/부분문자열.java
+++ Algorithm_문제풀이2021/src/day0505/부분문자열.java
@@ -26,10 +26,40 @@ public class 부분문자열 {
 
 		for(int i=1; i<LenOfPattern; i++) {      // i = 1부터
 			while(j > 0 && pattern.charAt(i) != pattern.charAt(j)) { // KMP알고리즘, 매칭되는 것 건너뛴다
-				j = pi[j-1];                                         // j를 재할당 (건너 뜀)
+				j = 0;//pi[j-1];                                         // j를 재할당 (건너 뜀)
 			}
 			if(pattern.charAt(i) == pattern.charAt(j)) {            // 접두사랑 접미사가 같다면
-				pi[i] = ++j;                                         // 해당 인덱스 카운트  (길이)
+				pi[i] = ++j;                                          // 해당 인덱스 카운트  (길이)
+			}
+		}
+		return pi;     // pi[] 반환
+	}
+
+	public static int[] getPi2(String pattern) {  // 접두사와 접미사 매칭 Count
+		int LenOfPattern = pattern.length();     // 찾을 Pattern's Length
+		int []pi = new int[LenOfPattern];        // pi[] 할당
+		int j = 0;                               // 패턴을 탐색할 인덱스
+
+		for(int i=1; i<LenOfPattern; i++) {      // i = 1부터
+			while(j > 0 && pattern.charAt(i) != pattern.charAt(j)) { // KMP알고리즘, 매칭되는 것 건너뛴다
+				j = 0;//pi[j-1];  j를 0이라 하면될것을 후..개같은 녀석들이  pi[j-1]이라 하는지 모르겠네;; // j를 재할당 (건너 뜀)
+			}
+			if(pattern.charAt(i) == pattern.charAt(j)) {            // 접두사랑 접미사가 같다면
+				pi[i] = ++j;                                          // 해당 인덱스 카운트  (길이)
+			}
+		}
+		return pi;     // pi[] 반환
+	}
+
+	public static int[] getPi3(String pattern) {  // 접두사와 접미사 매칭 Count
+		int m = pattern.length();     // 찾을 Pattern's Length
+		int []pi = new int[m];        // pi[] 할당
+
+		for(int begin = 1; begin < m; ++begin) {
+			for(int i = 0; i + begin < m; ++i) {
+				if(pattern.charAt(begin + i) != pattern.charAt(i))
+					break;
+				pi[begin + i] = Math.max(pi[begin+i], i+1);
 			}
 		}
 		return pi;     // pi[] 반환
@@ -39,7 +69,7 @@ public class 부분문자열 {
 	public static void KMP(String str, String pattern) {  // 매칭되는 str을 ArrayList에 저장
 
 		ArrayList<Integer> list = new ArrayList<>();      // 매칭된 str을 저장할 ArrayList
-		int [] pi = getPi(pattern);                       // 찾을 패턴의 접두사와 접미사가 카운트된 배열 불러온다
+		int [] pi = getPi2(pattern);                       // 찾을 패턴의 접두사와 접미사가 카운트된 배열 불러온다
 		int n = str.length();
 		int m = pattern.length();
 		int begin = 0, matched = 0;
