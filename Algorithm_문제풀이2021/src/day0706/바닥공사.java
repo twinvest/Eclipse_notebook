51b78c6151522a1d90fe34f19ea4b17a2d5218f5
KimTaeWoo <twinvest@naver.com>
2021-07-07 01:01:59 +0900

    개미전사, 바닥공사

---
+++ Algorithm_문제풀이2021/src/day0706/바닥공사.java
@@ -0,0 +1,18 @@
+package day0706;
+
+import java.util.Scanner;
+
+public class 바닥공사 {
+	static int[] dp = new int[1001];
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int N = scan.nextInt();
+		dp[0] = 0;
+		dp[1] = 1;
+		dp[2] = 3;
+		for(int i = 3; i<=N;++i) {
+			dp[i] = ((dp[i-2] * 2) + dp[i-1]) % 10007;
+		}
+		System.out.println(dp[N]);
+	}
+}
