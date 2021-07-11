1d0e3a68a8bf96a91b47b4254d46dc7d237a58d9
KimTaeWoo <twinvest@naver.com>
2021-07-11 21:04:31 +0900

    병사배치하기, 가장긴감소하는부분수열 ==> 같은 문제DP

---
+++ Algorithm_문제풀이2021/src/day0711/가장긴감소하는부분수열.java
@@ -0,0 +1,30 @@
+package day0711;
+
+import java.util.Scanner;
+
+public class 가장긴감소하는부분수열 {
+	static int N, ans;
+	static int[] dp, arr;
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		N = scan.nextInt();
+		arr = new int[N+1];
+		dp = new int[N+1];
+
+		for(int i = 1; i<=N;++i) {
+			arr[i] = scan.nextInt();
+			dp[i] = 1;
+		}
+		for(int i =1; i<=N; ++i) {
+			int cur = arr[i];
+			for(int j =1; j<=i; ++j) {
+				int comp = arr[j];
+				if(cur < comp) {
+					dp[i] = Math.max(dp[i], dp[j]+1);
+				}
+			}
+			ans = Math.max(ans, dp[i]);
+		}
+		System.out.println(ans);
+	}
+}
